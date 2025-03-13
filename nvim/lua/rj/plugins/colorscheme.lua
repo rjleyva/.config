834477365f0b24466daacd034c1899b8f9437275
RJL <rleyva@tutanota.com>
2025-03-13 02:50:46 -0400

    remove sonokai colorscheme config

--- nvim/lua/rj/plugins/colorscheme.lua
+++ nvim/lua/rj/plugins/colorscheme.lua
@@ -1,43 +1,27 @@
--- return {
---   "craftzdog/solarized-osaka.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     -- Load the colorscheme
---     vim.cmd([[colorscheme solarized-osaka]])
---
---     -- Default Settings for solarized-osaka
---     require("solarized-osaka").setup({
---       style = "dark",
---       transparent = true,
---       terminal_colors = true,
---       enable_italics = false,
---       styles = {
---         -- Style to be applied to different syntax groups
---         comments = { italic = true },
---         keywords = { italic = false },
---         functions = { bold = true },
---         variables = { italic = true },
---         string = { italic = false },
---         underline = true,
---         undercurl = true,
---       },
---     })
---   end,
--- }
-
 return {
-  {
-    "sainnhe/sonokai",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_transparent_background = "1"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "andromeda"
-      vim.g.sonokai_diagnostic_virtual_text = "colored"
-      vim.g.sonokai_diagnostic_line_highlight = "1"
-      vim.cmd.colorscheme("sonokai")
-    end,
-  },
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Load the colorscheme
+    vim.cmd([[colorscheme solarized-osaka]])
+
+    -- Default Settings for solarized-osaka
+    require("solarized-osaka").setup({
+      style = "dark",
+      transparent = true,
+      terminal_colors = true,
+      enable_italics = false,
+      styles = {
+        -- Style to be applied to different syntax groups
+        comments = { italic = true },
+        keywords = { italic = false },
+        functions = { bold = true },
+        variables = { italic = true },
+        string = { italic = false },
+        underline = true,
+        undercurl = true,
+      },
+    })
+  end,
 }
