c383d5bb170a5640d089cb5ff6794434dd62631d
RJL <rleyva@tutanota.com>
2025-03-13 02:12:01 -0400

    add sonokai colorscheme

--- nvim/lua/rj/plugins/colorscheme.lua
+++ nvim/lua/rj/plugins/colorscheme.lua
@@ -1,48 +1,43 @@
-return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Load the colorscheme
-    vim.cmd([[colorscheme solarized-osaka]])
-
-    -- Default Settings for solarized-osaka
-    require("solarized-osaka").setup({
-      style = "dark",
-      transparent = true,
-      terminal_colors = true,
-      enable_italics = false,
-      styles = {
-        -- Style to be applied to different syntax groups
-        comments = { italic = true },
-        keywords = { italic = false },
-        functions = { bold = true },
-        variables = { italic = true },
-        string = { italic = false },
-        underline = true,
-        undercurl = true,
-      },
-    })
-  end,
-}
-
 -- return {
---   "shaunsingh/nord.nvim",
+--   "craftzdog/solarized-osaka.nvim",
 --   lazy = false,
 --   priority = 1000,
 --   config = function()
---     -- Set the colorscheme
---     vim.cmd("colorscheme nord")
---
---     -- Configure  settings
---     vim.g.nord_contrast = true
---     vim.g.nord_borders = false
---     vim.g.nord_disable_background = true
---     vim.g.nord_italic = true
---     vim.g.nord_uniform_diff_background = false
---     vim.g.nord_bold = false
---
 --     -- Load the colorscheme
---     require("nord").set()
+--     vim.cmd([[colorscheme solarized-osaka]])
+--
+--     -- Default Settings for solarized-osaka
+--     require("solarized-osaka").setup({
+--       style = "dark",
+--       transparent = true,
+--       terminal_colors = true,
+--       enable_italics = false,
+--       styles = {
+--         -- Style to be applied to different syntax groups
+--         comments = { italic = true },
+--         keywords = { italic = false },
+--         functions = { bold = true },
+--         variables = { italic = true },
+--         string = { italic = false },
+--         underline = true,
+--         undercurl = true,
+--       },
+--     })
 --   end,
 -- }
+
+return {
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_transparent_background = "1"
+      vim.g.sonokai_enable_italic = "1"
+      vim.g.sonokai_style = "andromeda"
+      vim.g.sonokai_diagnostic_virtual_text = "colored"
+      vim.g.sonokai_diagnostic_line_highlight = "1"
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
+}
