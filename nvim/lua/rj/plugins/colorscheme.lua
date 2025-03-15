301d9f215ffeff44a6c0e0eca3e53c83a7404c13
RJL <rleyva@tutanota.com>
2025-03-15 15:25:40 -0400

    enable notify integration

--- nvim/lua/rj/plugins/colorscheme.lua
+++ nvim/lua/rj/plugins/colorscheme.lua
@@ -62,7 +62,7 @@ return {
         gitsigns = true,
         nvimtree = true,
         treesitter = true,
-        notify = false,
+        notify = true,
         mini = {
           enabled = true,
           indentscope_color = "",
